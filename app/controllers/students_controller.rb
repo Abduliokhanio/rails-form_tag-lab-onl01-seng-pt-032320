81dd8475e4ce5a4dd2f96804a345e9f9ea7dad3d
Abdul Khan <akhan17@outlook.com>
2020-07-17 22:40:37 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,4 +12,9 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def create
+  @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+  redirect_to student_path(@student)
+end
+
 end
